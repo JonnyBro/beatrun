9c8cf405d40c2e84c8f90f4dccd6a0d44f7aeae7
Jonny_Bro (Nikita) <niktom.nikto@yandex.ru>
2023-05-24 18:49:13 +0500

    now it's fixed

--- beatrun/gamemodes/beatrun/gamemode/cl/OnlineCourse.lua
+++ beatrun/gamemodes/beatrun/gamemode/cl/OnlineCourse.lua
@@ -1,5 +1,5 @@
-local apikey = CreateClientConVar("Beatrun_Apikey", "0", true, {FCVAR_ARCHIVE, FCVAR_UNLOGGED})
-local domain = CreateClientConVar("Beatrun_Domain", "localhost", true, {FCVAR_ARCHIVE, FCVAR_UNLOGGED})
+local apikey = CreateClientConVar("Beatrun_Apikey", "0", true, false, "Your API key")
+local domain = CreateClientConVar("Beatrun_Domain", "localhost", true, false, "Online courses domain")
 
 function UploadCourse()
 	if Course_Name == "" or Course_ID == "" then return print("Can't upload in Freeplay") end
