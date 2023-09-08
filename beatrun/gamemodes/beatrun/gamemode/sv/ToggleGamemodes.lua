58abf24f8a29de9ad9e7ba12faeb0adb03002418
Jonny_Bro (Nikita) <niktom.nikto@yandex.ru>
2023-09-08 17:49:37 +0500

    accept any gamemode string

--- beatrun/gamemodes/beatrun/gamemode/sv/ToggleGamemodes.lua
+++ beatrun/gamemodes/beatrun/gamemode/sv/ToggleGamemodes.lua
@@ -3,7 +3,7 @@ util.AddNetworkString("Beatrun_ToggleGamemode")
 net.Receive("Beatrun_ToggleGamemode", function(_, ply)
 	if not ply:IsAdmin() then return end
 
-	local gm = net.ReadString()
+	local gm = string.lower(net.ReadString())
 
 	if gm == "datatheft" then
 		if not GetGlobalBool("GM_DATATHEFT") then
