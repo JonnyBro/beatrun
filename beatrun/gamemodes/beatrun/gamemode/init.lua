453a6abbfc575308238d87ab1b36ca176badeb2f
Jonny_Bro (Nikita) <niktom.nikto@yandex.ru>
2023-09-21 18:43:32 +0500

    maybe this fixes something, dunno

--- beatrun/gamemodes/beatrun/gamemode/init.lua
+++ beatrun/gamemodes/beatrun/gamemode/init.lua
@@ -2,20 +2,20 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
-for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/cl/*.lua", "GAME", "nameasc")) do
+for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/sh/*.lua", "GAME")) do
+	print(v)
+
+	AddCSLuaFile("sh/" .. v)
+	include("sh/" .. v)
+end
+
+for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/cl/*.lua", "GAME")) do
 	print(v)
 
 	AddCSLuaFile("cl/" .. v)
 end
 
-for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/sh/*.lua", "GAME", "nameasc")) do
-	print(v)
-
-	include("sh/" .. v)
-	AddCSLuaFile("sh/" .. v)
-end
-
-for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/sv/*.lua", "GAME", "nameasc")) do
+for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/sv/*.lua", "GAME")) do
 	print(v)
 
 	include("sv/" .. v)
