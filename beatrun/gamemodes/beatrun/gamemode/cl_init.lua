629cbcb05eb2329dd57c8a1f7735a0317a7baf78
Jonny_Bro (Nikita) <niktom.nikto@yandex.ru>
2023-09-21 20:54:56 +0500

    :shrug:

--- beatrun/gamemodes/beatrun/gamemode/cl_init.lua
+++ beatrun/gamemodes/beatrun/gamemode/cl_init.lua
@@ -1,5 +1,5 @@
 include("shared.lua")
 
-for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/cl/*.lua", "GAME", "nameasc")) do
+for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/cl/*.lua", "GAME")) do
 	include("cl/" .. v)
 end
--- beatrun/gamemodes/beatrun/gamemode/init.lua
+++ beatrun/gamemodes/beatrun/gamemode/init.lua
@@ -2,17 +2,17 @@ AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 
+for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/cl/*.lua", "GAME")) do
+	print(v)
+
+	AddCSLuaFile("cl/" .. v)
+end
+
 for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/sh/*.lua", "GAME")) do
 	print(v)
 
 	AddCSLuaFile("sh/" .. v)
 	include("sh/" .. v)
-end
-
-for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/cl/*.lua", "GAME")) do
-	print(v)
-
-	AddCSLuaFile("cl/" .. v)
 end
 
 for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/sv/*.lua", "GAME")) do
