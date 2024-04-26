53b69e805846d5ce3f42e2f5494b76bedd51a378
Jonny_Bro (Nikita) <niktom.nikto@ya.ru>
2024-04-26 17:05:35 +0500

    remove unnecessary prints on load

--- beatrun/gamemodes/beatrun/gamemode/init.lua
+++ beatrun/gamemodes/beatrun/gamemode/init.lua
@@ -3,20 +3,16 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/cl/*.lua", "GAME")) do
-	print(v)
-
 	AddCSLuaFile("cl/" .. v)
 end
 
 for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/sh/*.lua", "GAME")) do
-	print(v)
-
 	AddCSLuaFile("sh/" .. v)
 	include("sh/" .. v)
 end
 
 for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/sv/*.lua", "GAME")) do
-	print(v)
-
 	include("sv/" .. v)
 end
+
+print("Beatrun gamemode loaded!")
