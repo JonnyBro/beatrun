714e4f787bb8a2838782160fe4e09e502c417a86
Jonny_Bro (Nikita) <niktom.nikto@ya.ru>
2024-07-24 14:23:03 +0500

    Write the version after first launch

--- beatrun/gamemodes/beatrun/gamemode/cl_init.lua
+++ beatrun/gamemodes/beatrun/gamemode/cl_init.lua
@@ -6,6 +6,10 @@ end
 
 http.Fetch("https://raw.githubusercontent.com/JonnyBro/beatrun/main/version.txt", function(body, size, headers, code)
 	if code == 200 then
+		if not file.Exists("beatrun/version.txt", "DATA") then
+			file.Write("beatrun/version.txt", body)
+		end
+
 		if body ~= VERSIONGLOBAL then
 			file.Write("beatrun/version.txt", body)
 		else
