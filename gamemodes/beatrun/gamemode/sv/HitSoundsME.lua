f241e79eba40a5a205dcfb2dfcf57aa83266ac8f
Jonny_Bro <48434875+JonnyBro@users.noreply.github.com>
2025-05-10 22:56:33 +0500

    refactor: remove viewpunch on bullet hit

--- gamemodes/beatrun/gamemode/sv/HitSoundsME.lua
+++ gamemodes/beatrun/gamemode/sv/HitSoundsME.lua
@@ -7,7 +7,7 @@ hook.Add("EntityTakeDamage", "MEHitSounds", function(ply, dmginfo)
 		if ply:GetVelocity():Length() > 400 then return true end -- Block damage if they're going very fast
 
 		ply:EmitSound("mirrorsedge/Flesh_0" .. tostring(math.random(1, 9)) .. ".wav")
-		ply:ViewPunch(Angle(math.Rand(-10, -5), 0, math.Rand(0, 5)))
+		-- ply:ViewPunch(Angle(math.Rand(-10, -5), 0, math.Rand(0, 5))) -- People cried so hard about this
 	elseif not ply:HasGodMode() and (dmginfo:IsFallDamage() and ply:Health() - dmginfo:GetDamage() <= 0) then
 		net.Start("DeathStopSound")
 		net.Send(ply)
