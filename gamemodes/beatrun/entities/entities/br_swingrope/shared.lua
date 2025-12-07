4051182e3b49dcba42c8014c2c6bd13605900ff6
Jonny <jonnyar226@gmail.com>
2025-12-07 19:24:07 +0500

    :shrug:

--- gamemodes/beatrun/entities/entities/br_swingrope/shared.lua
+++ gamemodes/beatrun/entities/entities/br_swingrope/shared.lua
@@ -83,8 +83,8 @@ function ENT:Draw()
 		if game.SinglePlayer() then
 			if grapplepos == self:GetStartPos() or grapplepos == self:GetEndPos() then return end
 		else
+			-- networked vectors loose precision, this doesnt happen in courses tho for some reason ??
 			if grapplepos:DistToSqr(self:GetStartPos()) < 0.001 or grapplepos:DistToSqr(self:GetEndPos()) < 0.001 then return end
-			--networked vectors loose precision, this doesnt happen in courses tho for some reason ??
 		end
 	end
 
