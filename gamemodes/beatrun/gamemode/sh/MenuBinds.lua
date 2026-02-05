4a75af64dfe5fa16e952415ef1cf776dc5e95bc5
Jonny <jonnyar226@gmail.com>
2026-02-05 15:15:45 +0500

    keybind for testing

--- gamemodes/beatrun/gamemode/sh/MenuBinds.lua
+++ gamemodes/beatrun/gamemode/sh/MenuBinds.lua
@@ -2,4 +2,8 @@ hook.Add("PlayerButtonDown", "CourseMenuBind", function(ply, button)
 	if (game.SinglePlayer() or CLIENT and IsFirstTimePredicted()) and button == KEY_F4 then
 		ply:ConCommand("Beatrun_CourseMenu")
 	end
+
+	if (game.SinglePlayer() or CLIENT and IsFirstTimePredicted()) and button == KEY_F3 then
+		ply:ConCommand("Beatrun_CoursesDatabase")
+	end
 end)
