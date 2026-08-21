c3a815bba8f73e3feb182031d142e4bfe39f610a
Global0Silver <Global0silver@gmail.com>
2026-08-21 10:48:34 +0300

    Fix Jumpturn on slippery surface

--- gamemodes/beatrun/gamemode/sh/Sliding.lua
+++ gamemodes/beatrun/gamemode/sh/Sliding.lua
@@ -398,7 +398,7 @@ hook.Add("SetupMove", "qslide", function(ply, mv, cmd)
 	local able_to_slide = ducking and sprinting and speed > runspeed * 0.5
 	local already_sliding = (ply:GetSlidingDelay() >= CT) or ply:GetSliding()
 
-	if not already_sliding and ply:Alive() and onground and not ply:GetJumpTurn() and (able_to_slide or slippery or ply:GetDive()) then
+	if not already_sliding and ply:Alive() and onground and (not ply:GetJumpTurn() or slippery) and (able_to_slide or slippery or ply:GetDive()) then
 		vel = math.min(speed, 541.44) * ply:GetOverdriveMult()
 
 		ParkourEvent(slippery and "slide45" or "slide", ply)
