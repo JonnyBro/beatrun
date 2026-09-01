b03f2dbe108b3f7f004dd1374da0f0f07e1b7f83
heckkkkk-jpg <hneck14@gmail.com>
2026-08-31 21:39:58 -0400

    More Bug Fixes + Extra Convars
    - Fixed Tosugeki Sound Volume.
    - Added Dive.Tosugeki into Sounds_Misc.
    -Added a janky method to prevent disarming on certain npcs IE Turrets,Helicopter, etc.
    - Added FallLock Convar.
    -Added Cloth.Fall on balance pipe.
    - Fixed crouch jumping when on a swingbar.
    - Fixed Stop Sound Convar.
    - Renamed "Beatrun_StopDeathSound" to "Beatrun_Death_StopSound"

--- gamemodes/beatrun/gamemode/cl/Death.lua
+++ gamemodes/beatrun/gamemode/cl/Death.lua
@@ -1,7 +1,9 @@
-local stopsound = CreateClientConVar("Beatrun_StopDeathSound", "1", true, false)
+local stopsound = CreateClientConVar("Beatrun_Death_StopSound", "0", true, false)
 
 net.Receive("DeathStopSound", function()
 	if stopsound:GetBool() then
+		return
+	else
 		RunConsoleCommand("stopsound")
 	end
 end)
